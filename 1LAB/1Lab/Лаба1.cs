8d720a1e8d1de70aeb5ebd87264d51dba4f93871
fedorstarykh <55690023+fedorstarykh@users.noreply.github.com>
2019-11-23 17:34:36 +0300

    Add files via upload
    added check for "the root is not a number" and "the root is-not a number "

---
+++ 1LAB/1Lab/Лаба1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Старых Федор ИУ5-35б";
+            double a;
+            double b;
+            double c;
+            if (args.Length == 3)
+            {
+                Console.WriteLine("Ввод ");
+                try
+                {
+                    a = Double.Parse(args[0]);
+                    b = Double.Parse(args[1]);
+                    c = Double.Parse(args[2]);
+                }
+                catch
+                {
+                    Console.WriteLine("Коэффициент должен быть числом. Повторите ввод. ");
+                    a = ReadDouble("Коэффициент а: ");
+                    b = ReadDouble("Коэффициент b: ");
+                    c = ReadDouble("Коэффициент c: ");
+                }
+            }
+            else
+            {
+                a = ReadDouble("Коэффициент a: ");
+                b = ReadDouble("Коэффициент b: ");
+                c = ReadDouble("Коэффициент c: ");
+            }
+            if (a == 0 && b != 0)//проверка на корень из минус числа добавлена
+            {
+                double root = (-1 * c) / b;
+                if (root > 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Корни: " + Math.Sqrt(root) + " и -" + Math.Sqrt(root));
+                }
+                if(root == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Корень: " + Math.Sqrt(root));
+                }
+                if(root < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Корни отсутствуют");
+                }
+                Console.ResetColor();
+            }
+            else if (a != 0)
+            {
+                double d = Math.Pow(b, 2) - 4 * a * c;
+                Console.WriteLine("Дискриминант: " + d);
+                if (d > 0)
+                {
+                    double root_1 = (-1 * b + Math.Sqrt(d)) / (2 * a);
+                    double root_2 = (-1 * b - Math.Sqrt(d)) / (2 * a);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    if (root_1 >= 0)
+                    {
+                        Console.WriteLine("Корень 1: " + Math.Sqrt(root_1));
+                        Console.WriteLine("Корень 2: " + -1 * Math.Sqrt(root_1));
+                    }
+                    if (root_2 >= 0)
+                    {
+                        Console.WriteLine("Корень 3: " + Math.Sqrt(root_2));
+                        Console.WriteLine("Корень 4: " + -1 * Math.Sqrt(root_2));
+                    }
+                }
+                else if (d == 0)
+                {
+                    double root = (b + Math.Sqrt(d)) / (2 * a);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Корень " + Math.Sqrt(root));
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Корни отсутствуют");
+                }
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("a и b = 0");
+            }
+            Console.ReadLine();
+        }
+        static double ReadDouble(string consoleMessage)
+        {
+            string resultString;
+            double resultDouble;
+            bool point;
+            do
+            {
+                Console.Write(consoleMessage);
+                resultString = Console.ReadLine();
+                if (!(point = double.TryParse(resultString, out resultDouble)))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Коэффициент должен быть числом. Повторите ввод.");
+                    Console.ResetColor();
+                }
+            }
+            while (!point);
+            return resultDouble;
+        }
+    }
+}
---
+++ 1LAB/Class1.cs
@@ -0,0 +1,129 @@
+﻿1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+
+	
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ax^2+bx+с
+            //D=b^2-4ac
+            double a, b, c;
+            Console.WriteLine("Введите a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите c: ");
+            c = Convert.ToDouble(Console.ReadLine());
+            Reshenie myclass = new Reshenie(a, b, c);
+
+
+        }
+    }
+    class Reshenie
+    {
+        private double a;
+        private double b;
+        private double c;
+        private double D;
+        private double x1;
+        private double x2;
+        public Reshenie(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public void raschet()
+        {
+            D = Math.Pow(b, 2) - 4 * a * c;
+            if (D > 0 || D == 0)
+            {
+                x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                Console.WriteLine("x1= {0}\n x2= {1}", Math.Sqrt(x1), Math.Sqrt(x2), '-'Math.Sqrt(x1),'-' Math.Sqrt(x2));
+                Console.ReadKey();
+
+            }
+
+
+            else
+            {
+                Console.WriteLine("Действительных корней нет");
+                Console.ReadKey();
+            }
+
+        }
+
+
+    }
+}
